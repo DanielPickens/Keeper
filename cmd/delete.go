98caa66ae6520e5d8838cde49ecba7de360fdc5c
daniel pickens <dpickens1989@gmail.com>
2023-01-09 00:19:03 -0700

    use value cmd struct in Go/CLI to declare the equivalent of get, delete structs for joining services through ingress Namespace inventory

---
+++ cmd/delete.go
@@ -0,0 +1,47 @@
+package cmd
+
+import (
+	"bytes"
+	"fmt"
+	"text/template"
+
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+// deleteCmd represents the delete command
+var deleteCmd = &cobra.Command{
+	Use:   "delete [command]",
+	Short: "Delete an object",
+	Long: `Delete resources by namespace or names.
+
+Deletion of a namespace will delete the namespace and remove all his attached object including the intentory attached to it. While removing an object will only supress it's metadata from the namespace but keep everything else.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		runDelete()
+	},
+}
+
+func NewDeleteCommand() *cobra.Command {
+	deleteCmd.AddCommand(NewDeleteJobCommand())
+	deleteCmd.AddCommand(NewDeleteNamespaceCommand())
+
+	return deleteCmd
+}
+
+func runDelete() {
+	tpl := template.Must(template.New("deleteCmd").Parse(`
+Using the get command with a sub-command is helpful. Please use one of the following sub-command :
+{{range . -}}
+- {{.}}
+{{end -}}
+`))
+
+	data := []string{"delete namespace", "delete job"}
+
+	contents := bytes.Buffer{}
+	if err := tpl.Execute(&contents, data); err != nil {
+		logrus.Fatalf("error while executing template : %v", err)
+	}
+
+	fmt.Println(contents.String())
+}
---
+++ cmd/get.go
@@ -0,0 +1,48 @@
+package cmd
+
+import (
+	"bytes"
+	"fmt"
+	"html/template"
+
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+var getCmd = &cobra.Command{
+	Use:   "get",
+	Short: "Show informations about a given namespace.",
+	Long: `This command display informations from a given namespace such as the list of exposed services
+or the url where you can join services throw ingress.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		runGet()
+	},
+}
+
+func NewGetCommand() *cobra.Command {
+	addCommonNamespaceCommandFlags(getCmd)
+
+	getCmd.AddCommand(NewGetNamespacesCommand())
+	getCmd.AddCommand(NewGetServicesCommand())
+
+	return getCmd
+}
+
+func runGet() {
+	tpl := template.Must(template.New("getCmd").Parse(`
+Using the get command with a sub-command is helpful. Please use one of the following sub-command :
+{{range .}}
+- {{.}}
+{{end}}
+`))
+
+	data := []string{"get services", "get namespaces"}
+
+	contents := bytes.Buffer{}
+	if err := tpl.Execute(&contents, data); err != nil {
+		logrus.Fatalf("error when executing template : %v", err)
+	}
+
+	fmt.Println(contents.String())
+
+}
