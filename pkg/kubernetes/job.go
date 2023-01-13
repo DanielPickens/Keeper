b95e24fe421c22fc73624c1464b563c584354b27
daniel pickens <dpickens1989@gmail.com>
2023-01-13 12:30:45 -0700

    pkg/kubernetes/job: pass go-client kubernetes client parameter for list batch job process

---
+++ pkg/kubernetes/job.go
@@ -0,0 +1,62 @@
+package kubernetes
+
+import (
+	"context"
+	"fmt"
+
+	"k8s.io/api/batch/v1"
+
+	"github.com/danielpickens/keeper/pkg/resource"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+)
+
+type jobRepository struct {
+	kubernetes kubernetes.Interface
+}
+
+// NewJobRepository returns a new JobRepository.
+// The parameter is a go-client kubernetes client.
+func NewJobRepository(kubernetes kubernetes.Interface) resource.JobRepository {
+	return &jobRepository{
+		kubernetes: kubernetes,
+	}
+}
+
+func (c *jobRepository) List(namespace string) (resource.Jobs, error) {
+	jl, err := c.kubernetes.BatchV1().Jobs(namespace).List(context.Background(), metav1.ListOptions{})
+
+	if err != nil {
+		return nil, fmt.Errorf("unable to list jobs: %v", err)
+	}
+
+	jobs := make(resource.Jobs, 0)
+
+	for _, job := range jl.Items {
+		status := resource.JobNotReady
+
+		if len(job.Status.Conditions) == 0 {
+			continue
+		}
+
+		if job.Status.Conditions[len(job.Status.Conditions)-1].Type == v1.JobComplete {
+			status = resource.JobReady
+		}
+
+		jobs = append(jobs, resource.Job{
+			Name:   job.Name,
+			Status: status,
+		})
+	}
+
+	return jobs, nil
+}
+
+func (c *jobRepository) Delete(namespace, resourceName string) error {
+	pp := metav1.DeletePropagationBackground
+	if err := c.kubernetes.BatchV1().Jobs(namespace).Delete(context.Background(), resourceName, metav1.DeleteOptions{PropagationPolicy: &pp}); err != nil {
+		return err
+	}
+	return nil
+}
