64a53b25723beb8c15b03ef8234c98209bd94288
Daniel Pickens <dpickens1989@gmail.com>
2023-01-19 12:38:17 -0700

    cmd/get_services: make display services information for ingress join services reproducible

---
+++ cmd/get_services.go
@@ -0,0 +1,59 @@
+package cmd
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"text/tabwriter"
+
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+var getServicesCmd = &cobra.Command{
+	Use:   "services",
+	Short: "Show information about exposed services from a given namespace.",
+	Long: `This command display informations from a given namespace such as the list of exposed services
+or the url where you can join services through ingress.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		err := runGetServices()
+		if err != nil {
+			logrus.Fatal(err.Error())
+		}
+
+	},
+}
+
+func NewGetServicesCommand() *cobra.Command {
+	addCommonNamespaceCommandFlags(getServicesCmd)
+	return getServicesCmd
+}
+
+func runGetServices() error {
+
+	if namespace == "" {
+		return errors.New("you must specify a namespace using the --namespace flag")
+	}
+
+	api := newAPI(newFileClient(playbookDir), newKubernetesClient())
+
+	// get exposed services (NodePort, LoadBalancer)
+	services, err := api.ListExposedServices(namespace)
+	if err != nil {
+		return errors.New(fmt.Sprintf("an error occurred when getting information about services : %v", err))
+	}
+
+	w := new(tabwriter.Writer)
+	w.Init(os.Stdout, 0, 8, 0, '\t', 0)
+	fmt.Fprintln(w, "Service Name\tAddress\tPort\tExposed Port\t")
+	for _, svc := range services {
+		for _, p := range svc.Ports {
+			fmt.Fprintf(w, fmt.Sprintf("%s\t%s\t%d\t%d\t\n", svc.Name, svc.Addr, p.Port, p.ExposedPort))
+		}
+	}
+	fmt.Fprintln(w)
+	w.Flush()
+
+	return nil
+
+}
